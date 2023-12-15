af4349a96eee5e9c740ed71a01d623cb94572bb2
unknown <Hyun BeomSeok>
2023-12-15 10:51:26 +0900

    1-27

--- src/main/java/com/example/first/answer/AnswerService.java
+++ src/main/java/com/example/first/answer/AnswerService.java
@@ -1,9 +1,11 @@
 package com.example.first.answer;
 
 import java.time.LocalDateTime;
+import java.util.Optional;
 
 import org.springframework.stereotype.Service;
 
+import com.example.first.DataNotFoundException;
 import com.example.first.question.Question;
 import com.example.first.user.SiteUser;
 
@@ -23,5 +25,24 @@ public class AnswerService {
         answer.setQuestion(question);
         answer.setAuthor(author);
         this.answerRepository.save(answer);
+    }
+
+    public Answer getAnswer(Integer id) {
+        Optional<Answer> answer = this.answerRepository.findById(id);
+        if (answer.isPresent()) {
+            return answer.get();
+        } else {
+            throw new DataNotFoundException("answer not found");
+        }
+    }
+
+    public void modify(Answer answer, String content) {
+        answer.setContent(content);
+        answer.setModifyDate(LocalDateTime.now());
+        this.answerRepository.save(answer);
+    }
+
+    public void delete(Answer answer) {
+        this.answerRepository.delete(answer);
     }
 }
